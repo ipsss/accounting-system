2b2a787cc33bb3dd9ec192928ec40c7814b5b141
Markchau <47062214+Markchau@users.noreply.github.com>
2019-04-26 21:57:52 +0800

    Add time setting functions

--- src/TimeSetting.h
+++ src/TimeSetting.h
@@ -1,8 +1,11 @@
 #ifndef __TIMESETTING_H__
 #define __TIMESETTING_H__
 
+//Function: To test input date within normal range or not
 extern bool DateRange(int,int,int);
+//Function: To test input month within normal range or not
 extern bool MonthRange(int);
+//Function: To test input year within normal range or not
 extern bool YearRange(int);
 
 #endif
