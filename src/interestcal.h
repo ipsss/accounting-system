9933519022bd48c3145e5bd9d282398b62b00cd7
Markchau <47062214+Markchau@users.noreply.github.com>
2019-04-27 03:04:14 +0800

    Interest calculator functions

--- src/interestcal.h
+++ src/interestcal.h
@@ -2,40 +2,51 @@
 #define __INTERESTCAL_H__
 #include <iostream>
 #include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
 using namespace std;
 
 class SINTERESTCAL {
   public:
-    string time;
-    SINTERESTCAL(string bank_no);  //default constructor of simple interest account
-  
+    //Constructor of simple interest account
+    SINTERESTCAL(string bank_no,double principal,double interest_rate,int duration,string time);
+
     //Accessor function
     string getBank_no();
-    double getInterestRate();
     double getPrincipal();
-    int getDuration();
-  
+    double getInterestRate();
+    double getDuration();
+    double getInterest();
+    double getTotalAmount();
+    string getTime();
+
     //Mutator function
-    void setBank_no(string bank_no);
-    void setInterestRate(double new_interest_rate);
+    void setBank_no(string new_bank_no);
     void setPrincipal(double new_principal);
+    void setInterestRate(double new_interest_rate);
     void setDuration(double new_duration);
-    
+    void setInterest(double new_interest);
+    void setTotalAmount(double new_total_amount);
+    void setTime(string new_time);
+
     //Function: User could choose the duration of deposit period
     void Durationselection(string time);
-  
-    //Function: To find simple interest of the input rate, principal and the deposit duration
-    double FindInterest(double interest_rate,double principal,int deposit_duration);
-  
-    //Function: File Output of the simple interest
-    void WriteInterest(double interest);
-  
+
+    //Function: To find simple interest of the input interest rate, principal and the deposit duration
+    double FindInterest(double principal,double interest_rate,double deposit_duration);
+
+    //Function: File Output. Report file of the results
+    void WriteInterest(string username);
+
   private:
-    const string bank_no;
-    double interest_rate;
+    string bank_no;
     double principal;
-    double interest; 
-    int deposit_duration;
+    double interest_rate;
+    double deposit_duration;
+    string time;
+    double interest;
+    double Total_Amount;
 };
 
 #endif
