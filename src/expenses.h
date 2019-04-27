2390ae67c804898adb872e3d13ecb1de5477c4f5
Markchau <47062214+Markchau@users.noreply.github.com>
2019-04-27 21:44:34 +0800

    Functions for expense records

--- src/expenses.h
+++ src/expenses.h
@@ -4,7 +4,9 @@
 #include <cstring>
 #include <vector>
 #include <fstream>
+#include <sstream>
 #include <iomanip>
+#include <algorithm>
 #include "account.h"
 #include "account.cpp"
 using namespace std;
@@ -12,19 +14,27 @@ using namespace std;
 class EXPENSES {
   public:
     unsigned int index;
-
+    
+    //Accessor
     vector<ACCOUNT> getExpenses();
     unsigned int getIndex();
+    
+    //Mutator
+    void setExpenses(vector<ACCOUNT>);
+    void setIndex(unsigned int);
 
-    void SearchExpenses(vector<ACCOUNT>);
-    void AddExpenses(ACCOUNT new_account);
-    void DeleteExpenses(unsigned int);
-    void EditExpenses(ACCOUNT new_account,unsigned int);
-    bool compare(ACCOUNT, ACCOUNT);
-    void SortExpenses(vector<ACCOUNT>);
-    void ReadFromExpenses(vector <ACCOUNT>);
-    void WriteToExpenses(vector<ACCOUNT>,string);
-    double TotalExpense(vector<ACCOUNT>)
+    bool SearchExpenses(vector<ACCOUNT>);  //Searching corresponding expense record
+    void AddExpenses(ACCOUNT new_account); //Add expense record
+    void DeleteExpenses(unsigned int);     //Delete the corresponding expense record
+    void EditExpenses(ACCOUNT new_account,unsigned int);  //Edit corresponding expense record
+
+    vector<ACCOUNT> SortExpenses(vector<ACCOUNT>); //Sort the expense records vector
+    bool compare(ACCOUNT left,ACCOUNT right);      //compare two expense records
+    void ReadFromExpenses(vector <ACCOUNT>);       //Show expense records
+  
+    //File Output. File storing all expense records from the user
+    //This function would create (update) the file called "(userid)_Expenses.txt"
+    void WriteToFile(string username,vector<ACCOUNT> Expenses);
 
   private:
     vector<ACCOUNT> Expenses;
