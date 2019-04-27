e2d495a60e0959f1705a61c7f2844d072344bc37
Markchau <47062214+Markchau@users.noreply.github.com>
2019-04-27 14:45:31 +0800

    Budget Setting Functions

--- src/BudgetSetting.h
+++ src/BudgetSetting.h
@@ -1,7 +1,7 @@
 #ifndef __BUDGETSETTING_H__
 #define __BUDGETSETTING_H__
 
-double budget_setting;
+double budget_setting=100000; //default budget setting is $100000 (upper limit)
 double new_budget_setting;
 extern double SetBudget(double&,double&); //Function: Set Budget
 extern void BudgetAlert(double,double); //Function: Output alert when Expenses exceed the Budget setting
